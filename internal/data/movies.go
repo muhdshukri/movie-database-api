d6a42e660469481860d0d814dd4b06888f4c26ce
muhdshukri <muhdshukri@gmail.com>
2024-09-24 16:00:52 +0800

    allow partial psql search and move migration files to cwd

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -28,9 +28,9 @@ type MovieModel struct {
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
 
 	query := `
- SELECT id, created_at, title, year, runtime,genres, version
+  SELECT id, created_at, title, year, runtime,genres, version
   FROM  movies
-  WHERE (LOWER(title)= LOWER($1) OR $1 = '')
+  WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
   AND (genres @> $2 OR $2 = '{}')
   ORDER BY id
   `
