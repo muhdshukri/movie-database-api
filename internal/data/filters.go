56c23ac66f2dab35bf31d02fb6b32b1ee090dbd9
muhdshukri <muhdshukri@gmail.com>
2024-09-27 17:24:43 +0800

    add paginating lists with pagination metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,9 +1,19 @@
 package data
 
 import (
-	"greenlight.m54h.dev/internal/validator"
+	"math"
 	"strings"
+
+	"greenlight.m54h.dev/internal/validator"
 )
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
 
 type Filters struct {
 	Page         int
@@ -12,8 +22,23 @@ type Filters struct {
 	SortSafeList []string
 }
 
-func ValidateFilters(v *validator.Validator, f Filters) {
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
 
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
+
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million_000_000")
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
@@ -39,5 +64,12 @@ func (f Filters) sortDirection() string {
 	}
 
 	return "ASC"
+}
 
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
